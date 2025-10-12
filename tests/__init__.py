c16693c6b9d778f8ac6cb5e1651082b50da0f66c
AbuAzad2025 <rafideen.ahmadghannam@gmail.com>
2025-10-12 22:01:40 +0300

    نظام طبي متكامل - Medical Center Management System v2.0

--- tests/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-مجموعة اختبارات النظام الصحي المتكامل
-Medical System Comprehensive Test Suite
-"""
-
-__version__ = "1.0.0"
-__author__ = "Azad Smart Systems"
-__description__ = "Comprehensive testing suite for the medical system"
